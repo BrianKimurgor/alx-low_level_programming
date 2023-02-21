42868820a25aa9477819c7de5578e511b1c41d93
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-02-21 13:51:14 -0800

    nested loop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,22 +3,28 @@
 #include <stdio.h>
 /**
  *  * main - Entry point
- *   * Return: Always return 0 (Success)
- *   */
+ *   * Return: Always return 0 (Success) 
+ */
 int main(void)
 {
-		int n;
+	int n;
+	int b;
 
-			int m;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	b = n % 10;
 
-				srand(time(0));
-					n = rand() - RAND_MAX / 2;
-						m = n % 10;
-							if (m > 5)
-										printf("Last digit of %d is %d and is greater than 5\n", n, m);
-								if (m == 0)
-											printf("Last digit of %d is %d and is 0\n", n, m);
-									if (m < 6 && m != 0)
-												printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-										return (0);
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and os 0\n", n, b);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6\n", n, b);
+	}
+	return (0);
 }
