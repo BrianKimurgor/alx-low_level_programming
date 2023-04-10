77f98e54a0e405c9fe5ed2284afdd2a836d19c55
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-04-10 16:43:30 -0700

    a function that creates a file.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,25 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
 /**
- * read_textfile - read text file and print th POSIX standard output.
+ * read_textfile - Read text file and print to POSIX standard output.
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: actual number of bytes read and printed
- *   0 when the function fails or is null
+ *         0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t read_count, write_count;
-	char *buffer;
+    int fd;
+    ssize_t read_count, write_count;
+    char *buffer;
 
-	if(filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	 if (fd == -1)
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
         return (0);
 
     buffer = malloc(sizeof(char) * letters);
