0ae2fd9e29c6a9dcf1ed3c5c92b1cff916605df6
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-04-10 18:05:27 -0700

    a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Entry point.
+ * @argc: The number of command line arguments.
+ * @argv: An array of the command line arguments.
+ *
+ * Return: 0 on success. 97, 98, 99 or 100 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int from_fd, to_fd;
+	ssize_t bytes_read;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(from_fd);
+		exit(99);
+	}
+	while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(to_fd, buffer, bytes_read) != bytes_read)
+		{
+
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(from_fd);
+			close(to_fd);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(from_fd);
+		close(to_fd);
+		exit(98);
+	}
+
+	if (close(from_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+		close(to_fd);
+		exit(100);
+	}
+
+	if (close(to_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+		exit(100);
+	}
+	return (0);
+}
